c7d408283ee0b98947895c606ba88d9cd4e15dcc
alejuy2018 <arielalejandrovegea@gmail.com>
2020-10-24 10:38:51 -0300

    Se ha agregado la funcionalidad de ecuacion a la calculadora y se realia PU

--- src/main/java/aplicacion/modelo/dominio/Calculadora.java
+++ src/main/java/aplicacion/modelo/dominio/Calculadora.java
@@ -1,6 +1,10 @@
 package aplicacion.modelo.dominio;
 
 public class Calculadora {
+    private Ecuacion ecuacion;
+
+
+
     public double sumar(double a, double b){
         return a+b;
     }
@@ -8,4 +12,14 @@ public class Calculadora {
     public double restar(double a, double b){
         return a-b;
     }
+
+    public Ecuacion getEcuacion(){
+        return this.ecuacion;
+    }
+
+    public void setEcuacion(Ecuacion ecuacion){
+        this.ecuacion = ecuacion;
+    }
+
+
 }
--- src/test/java/aplicacion/modelo/dominio/CalculadoraTest.java
+++ src/test/java/aplicacion/modelo/dominio/CalculadoraTest.java
@@ -35,4 +35,19 @@ public class CalculadoraTest {
         //evaluation
         assertEquals(2, calculadora.restar(a, b));
     }
+
+    @Test
+    public void evaluarEcuacionLineal(){
+        //define previus values
+        double a=5;
+        double b=7;
+        Ecuacion ecuacion = new EcuacionLineal(a,b);
+        //init
+        Calculadora calculadora = new Calculadora();
+        //define values
+        double x =1;
+        calculadora.setEcuacion(ecuacion);
+        //evaluation  
+        assertEquals(12d, calculadora.getEcuacion().resolverEcuacion(x));
+    }
 }
